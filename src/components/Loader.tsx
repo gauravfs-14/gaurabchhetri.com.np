7b8938c6646074e5d24b64e4ef966ba2e1fc1897
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2023-07-01 19:48:42 +0545

    Fix build fail #2

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -20,20 +20,32 @@ function Loader({ isLoading, setIsLoading }: any) {
             ease: "easeInOut",
           }}
         >
-      <motion.svg id="logo" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <title>Logo</title>
-        <motion.path initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{
-                    duration: 1,
-                    ease: "easeInOut",
-                    delay: 1,
-                  }}
-                  exit={{
-                    scale: 2,
-                  }}
-                  stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
-      </motion.svg>
+          <motion.svg
+            id="logo"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <title>Logo</title>
+            <motion.path
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 1,
+                ease: "easeInOut",
+                delay: 1,
+              }}
+              exit={{
+                scale: 2,
+              }}
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+            />
+          </motion.svg>
         </motion.div>
       )}
     </AnimatePresence>
