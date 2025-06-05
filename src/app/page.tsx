e2e289b0fa82b4bef6cdff10a5971f7c39c252d5
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2025-06-05 02:03:52 -0500

    feat: implement animated background component and enhance navbar with animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ProfileCard from "@/components/profile-card";
+import { AnimatedBackground } from "@/components/ui/animated-background";
 import { InView } from "@/components/ui/in-view";
 import {
   SiCss3,
@@ -91,34 +92,84 @@ export default function Home() {
       </InView>
       <div className="mt-14" />
       <div>
-        <p className="text-muted-foreground">
-          Hello World <span className="text-xl">👋</span>, I specialize in
-          building things that matters. Currently, I am an Undergraduate
-          Research Assistant at the Texas State University, where I combine my
-          skills in full-stack development with academic research to build
-          innovative tools and applications.
-        </p>
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: 20, filter: "blur(5px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 0.3, ease: "easeInOut", delay: 0.1 }}
+        >
+          <p className="text-muted-foreground">
+            Hello World <span className="text-xl">👋</span>, I specialize in
+            building things that matters. Currently, I am an Undergraduate
+            Research Assistant at the Texas State University, where I combine my
+            skills in full-stack development with academic research to build
+            innovative tools and applications.
+          </p>
+        </InView>
       </div>
       <div className="mt-14" />
       <div>
-        <h2 aria-description="Skills Section" className="font-bold text-lg">
-          {"// myToolBox"}
-        </h2>
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: 20, filter: "blur(5px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 0.3, ease: "easeInOut", delay: 0.1 }}
+        >
+          <h2 aria-description="Skills Section" className="font-bold text-lg">
+            {"// myToolBox"}
+          </h2>
+        </InView>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-          {skills.map((skill) => {
-            const Icon = skill.icon;
-            return (
-              <div
-                key={skill.name}
-                className="flex items-center space-x-2 px-3 py-4 rounded-xl hover:bg-card cursor-pointer transition-colors duration-200 ease-in-out shadow-sm hover:shadow-md"
-                title={skill.name}
-                aria-label={`Skill: ${skill.name}`}
-              >
-                <Icon className="w-6 h-6 text-primary" />
-                <span className="text-muted-foreground">{skill.name}</span>
-              </div>
-            );
-          })}
+          <AnimatedBackground
+            className="rounded-lg bg-zinc-100 dark:bg-zinc-800"
+            transition={{
+              type: "spring",
+              bounce: 0.2,
+              duration: 0.6,
+            }}
+            enableHover
+          >
+            {skills.map((skill, idx) => {
+              const Icon = skill.icon;
+              return (
+                <div
+                  key={idx}
+                  data-id={`card-${idx}`}
+                  className="cursor-pointer"
+                >
+                  <InView
+                    key={skill.name}
+                    variants={{
+                      hidden: { opacity: 0, y: 20, filter: "blur(5px)" },
+                      visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+                    }}
+                    viewOptions={{ margin: "0px 0px -200px 0px" }}
+                    transition={{
+                      duration: 0.3,
+                      ease: "easeInOut",
+                      delay: 0.3 + idx * 0.05,
+                    }}
+                  >
+                    <div
+                      key={skill.name}
+                      className="flex items-center space-x-2 px-3 py-4"
+                      title={skill.name}
+                      aria-label={`Skill: ${skill.name}`}
+                    >
+                      <Icon className="w-6 h-6 text-primary" />
+                      <span className="text-muted-foreground">
+                        {skill.name}
+                      </span>
+                    </div>
+                  </InView>
+                </div>
+              );
+            })}
+          </AnimatedBackground>
         </div>
       </div>
     </main>
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -2,48 +2,85 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { ExternalLink } from "lucide-react";
+import { InView } from "./ui/in-view";
 
 export default function NavBar() {
   return (
     <nav className="flex items-center justify-between p-4 pt-8 container mx-auto max-w-4xl bg-background/50 backdrop-blur-lg sticky top-0 z-50">
-      <div className="text-lg font-bold">
-        <Link href="/" className="">
-          {"<Gaurab />"}
-        </Link>
-      </div>
+      <InView
+        variants={{
+          hidden: { opacity: 0, y: -20, filter: "blur(5px)" },
+          visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+        }}
+        transition={{ duration: 0.5 }}
+        viewOptions={{ margin: "0px 0px -200px 0px" }}
+      >
+        <div className="text-lg font-bold">
+          <Link href="/" className="">
+            {"<Gaurab />"}
+          </Link>
+        </div>
+      </InView>
       <ul className="flex space-x-4 items-center">
-        <li>
-          <Link
-            href="#projects"
-            className={cn(
-              buttonVariants({ variant: "link", size: "lg" }),
-              "px-0 text-foreground hover:text-primary"
-            )}
-          >
-            projects
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="#blogs"
-            className={cn(
-              buttonVariants({ variant: "link", size: "lg" }),
-              "px-0 text-foreground hover:text-primary"
-            )}
-          >
-            blogs
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://resume.gaurabchhetri.com.np"
-            className={cn(buttonVariants({ variant: "default", size: "sm" }))}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            resume <ExternalLink className="inline ml w-4 h-4" />
-          </Link>
-        </li>
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: -20, filter: "blur(5px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+        >
+          <li>
+            <Link
+              href="#projects"
+              className={cn(
+                buttonVariants({ variant: "link", size: "lg" }),
+                "px-0 text-foreground hover:text-primary"
+              )}
+            >
+              projects
+            </Link>
+          </li>
+        </InView>
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: -20, filter: "blur(5px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+        >
+          <li>
+            <Link
+              href="#blogs"
+              className={cn(
+                buttonVariants({ variant: "link", size: "lg" }),
+                "px-0 text-foreground hover:text-primary"
+              )}
+            >
+              blogs
+            </Link>
+          </li>
+        </InView>
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: -20, filter: "blur(5px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+        >
+          <li>
+            <Link
+              href="https://resume.gaurabchhetri.com.np"
+              className={cn(buttonVariants({ variant: "default", size: "sm" }))}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              resume <ExternalLink className="inline ml w-4 h-4" />
+            </Link>
+          </li>
+        </InView>
       </ul>
     </nav>
   );
